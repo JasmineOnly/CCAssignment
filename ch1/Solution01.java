8e2b756eb9f9edfd229fced4a0cd8016dec7c841
yuanyuan <yuanyuam@andrew.cmu.edu>
2015-09-13 13:09:00 -0400

    Solution 01 02

---
+++ ch1/Solution01.java
@@ -0,0 +1,25 @@
+
+/*
+ * Author: Yuanyuan Ma
+ * Andrew ID: yuanyuam
+ * 
+ * Basic idea: sort the string first and then compare each term with the neighboring one 
+ **/
+
+public class Solution01 {
+	boolean isUniqueChars(String str){
+		
+		char[] sArr = str.toCharArray();
+		
+		java.util.Arrays.sort(sArr);
+		
+		for(int i = 0; i< sArr.length-1;i++){
+			if(sArr[i] == sArr[i+1]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+}
---
+++ ch1/Solution02.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Yuanyuan Ma
+ * Andrew ID: yuanyuam
+ * 
+ * Basic idea: sort the string first and then check whether they are equal to each other 
+ * Assume that the whitespace is significant
+ **/
+
+public class Solution02 {
+	
+	boolean permutation(String s, String t){
+		if (s.length() != t.length()){
+			return false;
+		}
+		
+		char[] sArr = s.toCharArray();
+		char[] tArr = t.toCharArray();
+		java.util.Arrays.sort(sArr);
+		java.util.Arrays.sort(tArr);
+		
+		return sArr.equals(tArr);
+		
+	}
+}
